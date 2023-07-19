b4ea150fa12c5e09864c1939cf0308ffdfc63465
simon <simonbakkenjantasuk@pm.me>
2023-07-19 22:03:11 +0200

    main.py -> haarcascade/main.py

---
+++ haarcascade/main.py
@@ -0,0 +1,38 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
+
+camera = cv2.VideoCapture(0)
+
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (int(camera.get(3)), int(camera.get(4))))
+
+while True:
+    ret, frame = camera.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+
+        smiles = smile_cascade.detectMultiScale(roi_gray, 1.7, 20)
+
+        for (sx, sy, sw, sh) in smiles:
+            cv2.rectangle(roi_color, (sx, sy), (sx + sw, sy + sh), (0, 0, 255), 2)
+
+    cv2.imshow('Face, Smile', frame)
+
+    out.write(frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+camera.release()
+out.release()  
+cv2.destroyAllWindows()
